c38f02dd5ce89dd1f545211312f1a8ec85f5209c
hydepark83 <hydepark83@gmail.com>
2013-07-10 21:47:23 +0000

    added web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 var str = fs.readFileSync('./index.html').toString();
 
-app.get('/', function(request, response,str) {
+app.get('/', function(request, response) {
 	  response.send(str);
 	});
 
