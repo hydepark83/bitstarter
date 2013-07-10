bdc4cdec7c616123fd3588840050a4511f87ea2f
hydepark83 <hydepark83@gmail.com>
2013-07-10 21:46:03 +0000

    added web.js

--- web.js
+++ web.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var file = fs.readFileSync('./index.html');
-var string = buf.toString(file);
+var str = fs.readFileSync('./index.html').toString();
 
-app.get('/', function(request, response, string) {
-  response.send(string);
-});
+app.get('/', function(request, response,str) {
+	  response.send(str);
+	});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
